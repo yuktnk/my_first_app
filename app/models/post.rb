4710ae8017a9fa9e0422754f88ef8a6aba4b55af
yuktnk <yuki0wl@gmail.com>
2020-09-02 23:27:24 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :post
 
   validates :image, presence: true
