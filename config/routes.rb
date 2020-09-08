119f49b766d704481a4e511a22fd58ff2bbd7620
yuktnk <yuki0wl@gmail.com>
2020-09-08 22:47:03 +0900

    [update]GuestLogin_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root "posts#index"
   resources :users, only: [:index, :new, :edit, :update, :show]
+  devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+  
+  root "posts#index"
   resources :posts do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
